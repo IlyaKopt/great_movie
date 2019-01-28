5cedb13e28bbd3bbbf71b444c3289dc578b9f524
ilya-k <kopturov.ilya@gmail.com>
2019-01-28 14:51:19 +0200

    Add data in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 1..10 do
+  User.create(username: "username_#{i.to_s}")
+end
