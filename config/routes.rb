0552ec937e2db5b662fd5ac0d4ba5438c0301b48
ilya-k <kopturov.ilya@gmail.com>
2019-01-29 13:21:51 +0200

    Add user api controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
     resources :users, only: %i[index show destroy]
     resources :movies, except: [:show]
   end
+
+  namespace 'api' do
+    resources :users, only: %i[show new create], default: :json
+  end
 end
